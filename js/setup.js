51d6185a92c4dada3545cf5ca6d477f9f5323a94
Loomb13 <lindone@live.ru>
2017-04-06 02:30:32 +0300

    исправлены ошибки тревиса

--- js/setup.js
+++ js/setup.js
@@ -9,10 +9,34 @@ var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 
-var WIZARD_NAMES = ['Иван','Хуан Себастьян','Мария', 'Кристоф','Виктор', 'Юлия','Люпита','Вашингтон'];
-var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко','Топольницкая', 'Нионго', 'Ирвинг'];
-
-
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var wizards = [
+  {
+    name: WIZARD_NAMES[0],
+    lastname: WIZARD_LASTNAMES[3],
+    coatColor:'rgb(215, 210, 55)'
+    eyesColor:'red'
+  },
+  {
+    name: WIZARD_NAMES[1],
+    lastname: WIZARD_LASTNAMES[5],
+    coatColor:'rgb(146, 100, 161)'
+    eyesColor:'blue'
+  },
+  {
+    name: WIZARD_NAMES[3],
+    lastname: WIZARD_LASTNAMES[6],
+    coatColor:'rgb(0, 0, 0)'
+    eyesColor:'green'
+  },
+  {
+    name: WIZARD_NAMES[6],
+    lastname: WIZARD_LASTNAMES[2],
+    coatColor:'rgb(56, 159, 117)'
+    eyesColor:'yellow'
+  }
+  ];
 
 for (var i = 0; i < wizards.length; i++); {
   var wizardElement = similarWizardTemplate.cloneNode(true);
