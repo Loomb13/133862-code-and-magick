c041601bdb24e879decbd0e69648c0dcac741024
Loomb13 <lindone@live.ru>
2017-04-07 14:31:10 +0300

    удавлена лишняя гистограмма2

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-window.renderStatistics = function (ctx, names,times) {
+window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -10,44 +10,44 @@ window.renderStatistics = function (ctx, names,times) {
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', 150,40);
+  ctx.fillText('Ура, вы победили!', 150, 40);
 
   var max = -1;
-  var maxIndex =-1;
+  var maxIndex = -1;
 
-   for(var i = 0 ; i < times.length; i++) {
-     var time = times[i];
-     if (time > max) {
-       max = time; //запись максимального времени
-       maxIndex = i;// индекс сохраняем для самого худшего времени
-     }
-   };
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time; // запись максимального времени
+      maxIndex = i;// индекс сохраняем для самого худшего времени
+    }
+  }
 
-  var histogramHeight =  40;
-  var step = histogramHeight /(max); //
+  var histogramHeight = 40;
+  var step = histogramHeight / (max); //
 
-  ctx.fillText('Список результатов:' + max.toFixed(0), 150,80);
+  ctx.fillText('Список результатов:' + max.toFixed(0), 150, 80);
 
-  ctx.textBaseline = "bottom";
+  ctx.textBaseline = 'bottom';
 
-  for (var i=0; i<times.length; i++ ) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     var name = names[i];
 
-    if (name == 'Вы') {
-      ctx.fillStyle ='rgba(255, 0, 0, 1)';
-    }
-    else { ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
-        ctx.fillRect(150,100,40,times[0] * step);
-      }
+    if (name === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
 
-      var initialX=200+ 50 *i;//px
-      var initialY=100;//px
-      var RectWidth=40;//px
-      var BottomIndent= 20;//px
-      ctx.fillRect (initialX, initialY, RectWidth, times[i] * step);
-      ctx.fillText (names[i], initialX, initialY+histogramHeight+BottomIndent);
     }
+
+    var initialX = 200 + 50 * i;// px
+    var initialY = 100;// px
+    var RectWidth = 40;// px
+    var BottomIndent = 20;// px
+    ctx.fillRect(initialX, initialY, RectWidth, times[i] * step);
+    ctx.fillText(names[i], initialX, initialY + histogramHeight + BottomIndent);
+  }
 
 };
 
