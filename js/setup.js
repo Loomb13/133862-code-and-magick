47a6a81e7f79bed110fa78895ceb60c76043b381
Loomb13 <lindone@live.ru>
2017-04-08 20:45:28 +0300

    some changes

--- js/setup.js
+++ js/setup.js
@@ -1,41 +1,34 @@
 'use strict';
 
 var userDialog = document.querySelector('.setup');
-//userDialog.classList.remove('hidden');
-//document.querySelector('.setup-similar').classList.remove('hidden');
-//var similarListElement = document.querySelector('.setup-similar-list');
-//var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+userDialog.classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
 
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'  ];
+var EYES = ['black', 'red', 'blue',  'yellow', 'green'];
 
-var wizards = [
+var randomName = Math.random(WIZARD_NAMES)* items.length;
+var randomLastname = Math.random (WIZARD_LASTNAMES)*items.length;
+var randomCoat = Math.random(COATS)*items.length;
+var randomEyes = Math.random (EYES)*items.length;
+
+function createRandomWizard () {return:
   {
-    name: WIZARD_NAMES[0],
-    lastname: WIZARD_LASTNAMES[3],
-    coatColor:'rgb(215, 210, 55)',
-    eyesColor:'red'
-  },
-  {
-    name: WIZARD_NAMES[1],
-    lastname: WIZARD_LASTNAMES[5],
-    coatColor:'rgb(146, 100, 161)',
-    eyesColor:'blue'
-  },
-  {
-    name: WIZARD_NAMES[3],
-    lastname: WIZARD_LASTNAMES[6],
-    coatColor:'rgb(0, 0, 0)',
-    eyesColor:'green'
-  },
-  {
-    name: WIZARD_NAMES[6],
-    lastname: WIZARD_LASTNAMES[2],
-    coatColor:'rgb(56, 159, 117)',
-    eyesColor:'yellow'
+    randomName,
+      randomLastname,
+      randomCoat,
+      randomEyes
   }
-  ];
+];
+
+
+
 
 for (var i = 0; i < wizards.length; i++); {
   var wizardElement = similarWizardTemplate.cloneNode(true);
